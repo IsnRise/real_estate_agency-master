0f1b7794884e2a03de56c38d2504e42dce34df5f
IsnRise <127597148+IsnRise@users.noreply.github.com>
2023-11-29 19:09:57 +0300

    Add files via upload

---
+++ property/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from .models import Flat, Complaint, Owner
+from django.contrib.auth.models import User
+
+
+@admin.register(Flat) 
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ['address','town', 'owner']
+    readonly_fields = ['created_at']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
+    list_editable = ['new_building']
+    list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    raw_id_fields = ('liked_by',)
+
+
+@admin.register(Complaint)
+class ComplainAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flat',)
+
+
+class FlatInline(admin.TabularInline):
+    model = Flat.owners.through
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
+    inlines = [
+        FlatInline
+    ]
